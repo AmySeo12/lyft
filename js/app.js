961099a86774a745d242c74a1f6eed1d8c354971
AmySeo12 <maryori.amy@gmail.com>
2016-10-12 17:12:49 -0500

    subiendo la parte tres

--- js/app.js
+++ js/app.js
@@ -3,10 +3,16 @@ var cargarPagina= function(){
 	$("#telefono").keyup(validarLongitud);
 	$("#siguiente").click(generarCódigo);
 	$("#registrarCodigo").click(registrarCodigo);
+	$(".codigo").keyup(focus);
+	$(".codigo").keypress(validar);
+	$(".codigo").keydown(validarNumeros);
+
+	$(".codigo").first().focus();
 }
 
 $(document).ready(cargarPagina);
 var codigoAleartorio = localStorage.getItem("codigo");
+var numero= localStorage.getItem("numeroTelefono");
 
 var validarNumeros= function(e){
 	var codigo = e.keyCode;
@@ -31,20 +37,45 @@ var generarCódigo= function(){
 		localStorage.removeItem("codigo");
 	}
 	if ($("#telefono").val().length == 9){
+		numero= $("#telefono").val();
 		codigoAleartorio= Math.floor(Math.random()*900)+100;
 		localStorage.setItem("codigo", codigoAleartorio);
 		alert("su código es: LAB-"+ codigoAleartorio);
+		localStorage.setItem("numeroTelefono", numero);
 	}
 }
 
 var registrarCodigo= function(){
+	var codigoConfirmacion= $("#codigo1").val() + $("#codigo2").val() + $("#codigo3").val();
 	if (codigoAleartorio !== null) {
-		if(codigoAleartorio == $("#codigo").val()){
+		if(codigoAleartorio == codigoConfirmacion){
 			$("#registrarCodigo").attr("href", "datos.html");
 		}else{
 			alert("Ingrese su código");
+			$(".codigo").last().focus();
 		}
 	} else {
 		alert("Genera tu código");
+	}
+}
+
+var focus= function(e){
+	var codigo = e.keyCode;
+	if(codigo >= 48 && codigo <= 57){
+		$(this).next().focus();
+		return true;
+	}else if(codigo == 8){
+		$(this).prev().focus();
+		return true;
+	}else{
+		return false;
+	}
+}
+
+var validar= function(e){
+	if($(this).val().length== 0){
+		return true;
+	}else{
+		return false;
 	}
 }
